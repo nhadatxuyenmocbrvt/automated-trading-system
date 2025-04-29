03e797042f7a65378a4b42fc99bf17ad72ffdb49
Nguyen Dinh Long <nguyendinhlongmmo@gmail.com>
2025-04-29 11:52:59 +0700

    Update 2025-04-29 11:52:59

--- auto_commit_push.bat
+++ auto_commit_push.bat
@@ -1,19 +1,25 @@
 @echo off
-set /p commit_message="Enter commit message: "
+:: Lấy ngày giờ chuẩn định dạng YYYY-MM-DD_HH-MM-SS
+for /f %%i in ('wmic os get localdatetime ^| find "."') do set dt=%%i
 
-:: Đảm bảo file .env được ignore
-if not exist ".gitignore" (
-    echo .env>>.gitignore
-)
+:: Tách thành ngày và giờ
+set year=%dt:~0,4%
+set month=%dt:~4,2%
+set day=%dt:~6,2%
+set hour=%dt:~8,2%
+set minute=%dt:~10,2%
+set second=%dt:~12,2%
 
-:: Add tất cả file đã cho phép (không add file .env nếu đã ignore)
+:: Tạo commit message tự động
+set commit_message=Update %year%-%month%-%day% %hour%:%minute%:%second%
+
+:: In commit message ra màn hình
+echo Commit Message: %commit_message%
+
+:: Chạy git
 git add .
-
-:: Hiển thị trạng thái để kiểm tra lần nữa
-git status
-
-:: Commit với message
 git commit -m "%commit_message%"
-
-:: Push lên remote
 git push
+
+:: Thoát CMD
+exit
