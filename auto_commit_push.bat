ea12b2745784c939429c8560dc0cc7f1cbaff343
Nguyen Dinh Long <nguyendinhlongmmo@gmail.com>
2025-04-29 11:35:39 +0700

    new

--- auto_commit_push.bat
+++ auto_commit_push.bat
@@ -1,6 +1,19 @@
 @echo off
+set /p commit_message="Enter commit message: "
+
+:: Đảm bảo file .env được ignore
+if not exist ".gitignore" (
+    echo .env>>.gitignore
+)
+
+:: Add tất cả file đã cho phép (không add file .env nếu đã ignore)
 git add .
-git commit -m "Auto commit %date% %time%"
-git push origin main
-echo Auto commit va push thanh cong!
-pause
+
+:: Hiển thị trạng thái để kiểm tra lần nữa
+git status
+
+:: Commit với message
+git commit -m "%commit_message%"
+
+:: Push lên remote
+git push
