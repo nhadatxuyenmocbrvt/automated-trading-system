52267b8454c750304450c0ece1139d5c16f43fa7
Nguyen Dinh Long <nguyendinhlongmmo@gmail.com>
2025-04-29 11:58:27 +0700

    y

--- auto_commit_push.bat
+++ auto_commit_push.bat
@@ -1,18 +1,5 @@
 @echo off
-:: Lấy thời gian hiện tại
-for /f "tokens=1-4 delims=/ " %%a in ('date /t') do set mydate=%%d-%%b-%%a
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do set mytime=%%a:%%b
-
-:: Tạo commit message tự động
-set commit_message=Update %mydate% %mytime%
-
-:: Hiển thị commit message
-echo Commit Message: %commit_message%
-
-:: Tiến hành git
+set /p commit_message="Enter commit message: "
 git add .
 git commit -m "%commit_message%"
 git push
-
-:: Thoát sau khi hoàn thành
-exit
